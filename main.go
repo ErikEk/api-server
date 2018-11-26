3f60a236b9131bb4e01968249f9c8e2882e619e3
ErikEk <erik.ek@gmail.com>
2018-11-26 20:21:21 +0100

    first main

---
+++ main.go
@@ -0,0 +1,208 @@
+package main
+
+import (
+	//"bytes"
+	//"database/sql"
+	"fmt"
+	"net/http"
+	"encoding/json"
+	"log"
+
+	"github.com/gorilla/mux"
+	//_ "github.com/go-sql-driver/mysql"
+)
+
+
+
+
+type Person struct {
+	ID        string   `json:"id,omitempty`
+	Firstname string   `json:"firstname,omitempty`
+	Lastname  string   `json:"lastname,omitempty"`
+	Address   *Address `json:"address,omitempty"`
+}
+type Address struct {
+	City  string `json:"city,omitempty"`
+	State string `json:"state,omitempty"`
+}
+
+var people []Person
+
+func GetPersonEndpoint(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	for _, item := range people {
+		if item.ID == params["id"] {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(&Person{})
+}
+func GetPeopleEndpoint(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode(people)
+}
+func CreatePersonEndpoint(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	fmt.Println(params)
+	var person Person
+	_ = json.NewDecoder(r.Body).Decode(&person)
+	person.ID = params["id"]
+	people = append(people, person)
+	json.NewEncoder(w).Encode(people)
+}
+func DeletePersonEndpoint(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	for index, item := range people {
+		if item.ID == params["id"] {
+			people = append(people[:index], people[index+1:]...)
+			break
+		}
+		json.NewEncoder(w).Encode(people)
+	}
+}
+
+func main() {
+	router := mux.NewRouter()
+	people = append(people, Person{ID: "1", Firstname: "John", Lastname: "Doe", Address: &Address{City: "City X", State: "State X"}})
+	people = append(people, Person{ID: "2", Firstname: "Koko", Lastname: "Doe", Address: &Address{City: "City Z", State: "State Y"}})
+	router.HandleFunc("/people", GetPeopleEndpoint).Methods("GET")
+	router.HandleFunc("/people/{id}", GetPersonEndpoint).Methods("GET")
+	router.HandleFunc("/people/{id}", CreatePersonEndpoint).Methods("POST")
+	router.HandleFunc("/people/{id}", DeletePersonEndpoint).Methods("DELETE")
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
+
+/*
+func main() {
+	db, err := sql.Open("mysql", "root:passapp@tcp(127.0.0.1:3306)/sys")
+	if err != nil {
+		fmt.Print(err.Error())
+	}
+	defer db.Close()
+	// make sure connection is available
+	err = db.Ping()
+	if err != nil {
+		fmt.Print(err.Error())
+	}
+	type Person struct {
+		Id         int
+		First_Name string
+		Last_Name  string
+	}
+	router := mux.NewRouter()
+
+	// GET a person detail
+	router.GET("/person/:id", func(c *gin.Context) {
+		
+		id := c.Param("id")
+		row := db.QueryRow("select id, first_name, last_name from person where id = ?;", id)
+		err = row.Scan(&person.Id, &person.First_Name, &person.Last_Name)
+		if err != nil {
+			// If no results send null
+			result = gin.H{
+				"result": nil,
+				"count":  0,
+			}
+		} else {
+			result = gin.H{
+				"result": person,
+				"count":  1,
+			}
+		}
+		//c.JSON(http.StatusOK, result)
+	})
+	
+	// GET all persons
+	router.GET("/persons", func(c *gin.Context) {
+		var (
+			person  Person
+			persons []Person
+		)
+		rows, err := db.Query("select id, first_name, last_name from person;")
+		if err != nil {
+			fmt.Print(err.Error())
+		}
+		for rows.Next() {
+			err = rows.Scan(&person.Id, &person.First_Name, &person.Last_Name)
+			persons = append(persons, person)
+			if err != nil {
+				fmt.Print(err.Error())
+			}
+		}
+		defer rows.Close()
+		c.JSON(http.StatusOK, gin.H{
+			"result": persons,
+			"count":  len(persons),
+		})
+	})
+
+	// POST new person details
+	router.POST("/person", func(c *gin.Context) {
+		var buffer bytes.Buffer
+		first_name := c.PostForm("first_name")
+		last_name := c.PostForm("last_name")
+		stmt, err := db.Prepare("insert into person (first_name, last_name) values(?,?);")
+		if err != nil {
+			fmt.Print(err.Error())
+		}
+		_, err = stmt.Exec(first_name, last_name)
+
+		if err != nil {
+			fmt.Print(err.Error())
+		}
+
+		// Fastest way to append strings
+		buffer.WriteString(first_name)
+		buffer.WriteString(" ")
+		buffer.WriteString(last_name)
+		defer stmt.Close()
+		name := buffer.String()
+		c.JSON(http.StatusOK, gin.H{
+			"message": fmt.Sprintf(" %s successfully created", name),
+		})
+	})
+
+	// PUT - update a person details
+	router.PUT("/person", func(c *gin.Context) {
+		var buffer bytes.Buffer
+		id := c.Query("id")
+		first_name := c.PostForm("first_name")
+		last_name := c.PostForm("last_name")
+		stmt, err := db.Prepare("update person set first_name= ?, last_name= ? where id= ?;")
+		if err != nil {
+			fmt.Print(err.Error())
+		}
+		_, err = stmt.Exec(first_name, last_name, id)
+		if err != nil {
+			fmt.Print(err.Error())
+		}
+
+		// Fastest way to append strings
+		buffer.WriteString(first_name)
+		buffer.WriteString(" ")
+		buffer.WriteString(last_name)
+		defer stmt.Close()
+		name := buffer.String()
+		c.JSON(http.StatusOK, gin.H{
+			"message": fmt.Sprintf("Successfully updated to %s", name),
+		})
+	})
+
+	// Delete resources
+	router.DELETE("/person", func(c *gin.Context) {
+		id := c.Query("id")
+		stmt, err := db.Prepare("delete from person where id= ?;")
+		if err != nil {
+			fmt.Print(err.Error())
+		}
+		_, err = stmt.Exec(id)
+		if err != nil {
+			fmt.Print(err.Error())
+		}
+		c.JSON(http.StatusOK, gin.H{
+			"message": fmt.Sprintf("Successfully deleted user: %s", id),
+		})
+	})
+	
+	router.Run(":8080")
+}*/
